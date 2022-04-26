be2ed4ad7a4cc0c24ad017cbdb66e9aa0f2defb0
booldook <booldook@gmail.com>
2022-04-26 17:38:34 +0900

    book-slice 1차 완성

--- src/store/slice/book-slice.js
+++ src/store/slice/book-slice.js
@@ -5,18 +5,19 @@ import { retrieveBookApi } from "store/api/book-api";
 const name = 'book';
 
 const initialState = {
+  isLoading: false,
   query: '',
   isEnd: false,
   totalCount: 0,
   books: [],
-  err: ''
+  err: null
 }
 
 /* async action */
 export const retrieveBook = createAsyncThunk(`${name}/retrieveBook`, async (data, thunkApi) => {
   try {
     const { data: { documents, meta } } = await retrieveBookApi(data);
-    return { documents, meta };
+    return { books: documents, isEnd: meta.is_end, totalCount: meta.total_count };
   }
   catch(err) {
     return thunkApi.rejectWithValue(err.response.data);
@@ -24,27 +25,46 @@ export const retrieveBook = createAsyncThunk(`${name}/retrieveBook`, async (data
 })
 
 
-/* reducer */
+/* sync reducer */
 const reducers = {
   resetBook(state, action) {
     // state = {...initialState}; // books x 
     Object.assign(state, initialState);
+  },
+  setQuery(state, action) {
+    state.query = action.payload;
   }
 }
 
 
 /* extraReducer */
+const extraReducers = builder => builder
+.addCase(retrieveBook.pending, (state, action) => {
+  state.isLoading = true;
+})
+.addCase(retrieveBook.fulfilled, (state, action) => {
+  state.isLoading = false;
+  state.books = action.payload.books;
+  state.isEnd = action.payload.isEnd;
+  state.totalCount = action.payload.totalCount;
+})
+.addCase(retrieveBook.rejected, (state, action) => {
+  state.isLoading = false;
+  state.err = action.payload;
+})
 
 
 /* createSlice */
-const bookSlice = createSlice({ initialState });
+const bookSlice = createSlice({ name, initialState, reducers, extraReducers });
 
 
 /* user Getter */
 
 
-/* export */
 
+/* export */
+export const { resetBook, setQuery } = bookSlice.actions
+export default bookSlice;
 
 
 
